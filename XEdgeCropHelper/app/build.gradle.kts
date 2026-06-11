cb1085889de3800a35e6006440e75cfe37ff34df
trup40 <trup40@protonmail.com>
2026-06-11 22:23:55 +0300

    exclude app from Recents...

--- XEdgeCropHelper/app/build.gradle.kts
+++ XEdgeCropHelper/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.trup40.xedgecrop"
         minSdk = 34
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
